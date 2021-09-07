0ae3b6b4a8228da8449f231b5221aea840f00bdf
aiyu666 <zrobin8741@gmail.com>
2021-09-08 01:41:58 +0800

    Fix filter rent data casue by api upgrade

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const http = require('http');
 
 const { getRequest } = require('./lib/request');
 const sendLineNotify = require('./lib/sendLineNotify');
+const getFirstPostId = require('./lib/getFirstPostId');
 const getToken = require('./lib/getToken');
 
 let nowTimestamp = Math.floor(Date.now() / 1000);
 let stopIntervalId;
 let countFail = 0;
-let serviceStatus = process.env.SERVICE_STATUS || 'true';
-
-(() => {
-  if(serviceStatus !== 'true') return console.log('Default service status is false.');
+(async () => {
+  // let originPostId = await getFirstPostId();
+  let originPostId = 1;
   stopIntervalId = setInterval(async () => {
     const headerInfo = await getToken(process.env.TARGET_URL);
     const houseListURL = `https://rent.591.com.tw/home/search/rsList?${process.env.TARGET_URL.split('?')[1]}`;
@@ -34,17 +34,15 @@ let serviceStatus = process.env.SERVICE_STATUS || 'true';
       });
       if(resp.statusCode !== 200) throw `Token 可能過期了，目前 StatusCode: ${resp.statusCode}`;
       const { data } = resp.body.data;
-      const targetData = data.filter((post) => post.updatetime > nowTimestamp);
-      if (targetData.length === 0) return;
-      nowTimestamp = targetData[0].updatetime;
-      for (let i = 0; i < targetData.length; i += 1) {
-        await sendLineNotify(`\nhttps://rent.591.com.tw/rent-detail-${targetData[i].id}.html`, process.env.LINE_NOTIFY_TOKEN);
-      }
+      const targetData = (data[0].post_id !== originPostId) ? data[0].post_id : originPostId;
+      if (targetData === originPostId) return;
+      await sendLineNotify(`\nhttps://rent.591.com.tw/rent-detail-${targetData}.html`, process.env.LINE_NOTIFY_TOKEN);
+      originPostId = targetData;
     } catch (error) {
       if(countFail > 10) {
         await sendLineNotify(`\n好像出事了! 但是我嘗試重新拿 Token 第 ${countFail} 次了所以暫時先把程式關閉，有空可以檢查一下。\n `, process.env.LINE_NOTIFY_TOKEN);
         clearInterval(stopIntervalId);
-      }  
+      };
       console.error(`Fetch the 591 rent fail: ${error}`);
       countFail += 1;
     }
